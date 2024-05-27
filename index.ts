b03914b4adfa123b4143b9cd6a00c41f0aee7506
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2024-05-27 15:40:04 -0400

    removed comments

--- index.ts
+++ index.ts
@@ -24,29 +24,27 @@ async function main() {
         password: config.LINKEDIN_PASSWORD
       });
 
-      // const formData: ApplicationFormData = {
-      //   phone: config.PHONE,
-      //   cvPath: config.CV_PATH,
-      //   homeCity: config.HOME_CITY,
-      //   coverLetterPath: config.COVER_LETTER_PATH,
-      //   yearsOfExperience: config.YEARS_OF_EXPERIENCE,
-      //   languageProficiency: config.LANGUAGE_PROFICIENCY,
-      //   requiresVisaSponsorship: config.REQUIRES_VISA_SPONSORSHIP,
-      //   booleans: config.BOOLEANS,
-      //   textFields: config.TEXT_FIELDS,
-      //   multipleChoiceFields: config.MULTIPLE_CHOICE_FIELDS,
-      // };
+      const formData: ApplicationFormData = {
+        phone: config.PHONE,
+        cvPath: config.CV_PATH,
+        homeCity: config.HOME_CITY,
+        coverLetterPath: config.COVER_LETTER_PATH,
+        yearsOfExperience: config.YEARS_OF_EXPERIENCE,
+        languageProficiency: config.LANGUAGE_PROFICIENCY,
+        requiresVisaSponsorship: config.REQUIRES_VISA_SPONSORSHIP,
+        booleans: config.BOOLEANS,
+        textFields: config.TEXT_FIELDS,
+        multipleChoiceFields: config.MULTIPLE_CHOICE_FIELDS,
+      };
       
 
-      // const links = await findJobs(listingPage,config.KEYWORDS)
+      const links = await findJobs(listingPage,config.KEYWORDS)
       
-      // // links.forEach((link) => {
-      // //   applyToJob(listingPage,link,formData)
-      // // })
+      links.forEach((link) => {
+        applyToJob(listingPage,link,formData)
+      })
 
-      // await applyToJob(listingPage, `https://www.linkedin.com/jobs/view/3919321857/?eBP=CwEAAAGPtzVyrMmFCxoteXruehaDuJeeQqKrv3r2B7TCEpBsIwT3aDXg8WzYAlDC9_OOVZX3IL6SjL1-pZNw5fsvrzE9qnIX8JXkZnqPUBOKOYhQPgKPAPovoLQdnRlJcdT9BHJIgFvUae-6kwZTfyxuxVosalyW3WrMTniIvShbHcHMWXIFqt_aklSFLJwEiIM9jR-r8aYb8DH0Yn1Aajl0JGxIV7ZpnGTHv2xuHKsvVGxt-OSV3JKJ-LlaKIuKSILH1r1-Aso5kW4cdlHU8rmAMsoeIvVV8QT6XgVdG4r2aoEZQOeWkjK9bQmjx4EaYZIquTPlQGrfjbwMubRX_EX_gNmnMXKd5kj8LQN0quCmGt5HiZQyXt_bi67GpG6czwSCesaQrttN67RH7lp6PEJO8V7wuI0yR-04GysUtA99jrEMwx766rtE_G64QxY7ziaOhes1quKFVzC7W8XFakzk4dECvkmzDw&refId=00YPpgTZqHzJsZokn%2BoTVw%3D%3D&trackingId=Bn8K0Jh5vmUxBlCY0VSdAA%3D%3D&trk=flagship3_search_srp_jobs`,formData)
-
-      // // listingPage.close()
+      listingPage.close()
 }
 
 main()
